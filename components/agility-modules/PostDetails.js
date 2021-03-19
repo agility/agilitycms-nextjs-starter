51093caead32d78a902cdd90df41e1c166fdba2a
Joshua Isaac <hello.joshua@hotmail.com>
2021-03-19 09:44:36 -0400

    fix blog spacing

--- components/agility-modules/PostDetails.js
+++ components/agility-modules/PostDetails.js
@@ -23,38 +23,34 @@ const PostDetails = ({ dynamicPageItem }) => {
   }
 
   return (
-    <div className="max-w-xl mx-auto my-12 md:my-18 lg:my-20 px-6">
-      <div className="">
-        <Image
-          src={post.image.url}
-          alt={post.title}
-          width="1000"
-          height="1000"
-          className="rounded-md"
-        />
-        <div className="flex items-center mt-2 mb-8">
-          <div>
-            <img
-              src={
-                author ? author.fields.image.url : "https://picsum.photos/200"
-              }
-              width="50"
-              height="50"
-              className="rounded-md"
-            />
-          </div>
-          <div className="pl-2">
-            <p className="font-medium">
-              {author ? author.fields.name : "Unknown"}
-            </p>
-            <p className="text-gray-400 text-sm">Published: {dateStr}</p>
-          </div>
+    <div className="max-w-2xl mx-auto my-12 md:my-18 lg:my-20 px-6">
+      <Image
+        src={post.image.url}
+        alt={post.title}
+        width="1000"
+        height="1000"
+        className="rounded-md"
+      />
+      <div className="flex items-center mt-2 mb-8">
+        <div>
+          <img
+            src={author ? author.fields.image.url : "https://picsum.photos/200"}
+            width="50"
+            height="50"
+            className="rounded-md"
+          />
         </div>
-        <p className="text-indigo-700 font-medium text-sm uppercase mb-2">
-          {category}
-        </p>
-        <h1 className="text-4xl font-bold mb-4">{post.title}</h1>
+        <div className="pl-2">
+          <p className="font-medium">
+            {author ? author.fields.name : "Unknown"}
+          </p>
+          <p className="text-gray-400 text-sm">Published: {dateStr}</p>
+        </div>
       </div>
+      <p className="text-indigo-700 font-medium text-sm uppercase mb-2">
+        {category}
+      </p>
+      <h1 className="text-4xl font-bold mb-4">{post.title}</h1>
       <div
         className="prose max-w-full"
         dangerouslySetInnerHTML={renderHTML(post.content)}

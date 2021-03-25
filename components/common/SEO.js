2cd019bcd9c96438e133553aaacc7bc7126cf86c
Joshua Isaac <hello.joshua@hotmail.com>
2021-03-25 10:28:49 -0400

    added google fonts

--- components/common/SEO.js
+++ components/common/SEO.js
@@ -10,12 +10,16 @@ const SEO = ({ title, description, keywords, ogImage, metaHTML }) => {
   }
   return (
     <Head>
-      <title>{title} - Agility CMS Sample Blog</title>
+      <title>{title} | Agility CMS Sample Travel Blog</title>
       <meta name="generator" content="Agility CMS" />
       <meta name="agility_timestamp" content={new Date().toLocaleString()} />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta name="description" content={description} />
       <meta name="keywords" content={keywords} />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;900&display=swap"
+        rel="stylesheet"
+      />
       {ogImage && <meta property="og:image" content={ogImage} />}
       {additionalHeaderMarkup}
     </Head>

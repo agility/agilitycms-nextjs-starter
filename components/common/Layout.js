1c4bb666749abdfa28deaa486af4d90c458aadab
Joshua Isaac <hello.joshua@hotmail.com>
2021-03-26 16:12:39 -0400

    testing moving handle preview to layout

--- components/common/Layout.js
+++ components/common/Layout.js
@@ -7,6 +7,42 @@ import LoadingWidget from "./LoadingWidget";
 import { useRouter } from "next/router";
 import Error from "next/error";
 
+const getParameterByName = (name) => {
+  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
+  var results = regex.exec(location.search);
+  return results === null
+    ? ""
+    : decodeURIComponent(results[1].replace(/\+/g, " "));
+};
+
+const handlePreview = () => {
+  if (!process.browser) {
+    //kickout if this is not being executed in the browser
+    return false;
+  }
+  const agilityPreviewKey = getParameterByName(`agilitypreviewkey`);
+  if (!agilityPreviewKey) {
+    //kickout if we don't have a preview key
+    return false;
+  }
+  //redirect this to our preview API route
+  const previewAPIRoute = `/api/preview`;
+  let previewAPIUrl = `${previewAPIRoute}?slug=${window.location.pathname}&agilitypreviewkey=${agilityPreviewKey}`;
+  const dynamicPageContentID = parseInt(
+    getParameterByName("ContentID") ?? getParameterByName("contentID")
+  );
+  if (dynamicPageContentID > 0) {
+    previewAPIUrl += `&ContentID=${dynamicPageContentID}`;
+  }
+  console.log("Activating preview", previewAPIUrl);
+  //do the redirect
+  setTimeout(function () {
+    window.location.href = previewAPIUrl;
+  }, 100000000);
+  return true;
+};
+
 function Layout(props) {
   const {
     page,
@@ -32,6 +68,10 @@ function Layout(props) {
 
   if (dynamicPageItem?.seo?.metaDescription) {
     page.seo.metaDescription = dynamicPageItem.seo.metaDescription;
+  }
+
+  if (handlePreview()) {
+    return <LoadingWidget message="Loading Preview" />;
   }
 
   return (

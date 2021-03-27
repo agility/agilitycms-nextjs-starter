18f0479e6a2cabe90feef76da48af13968e3eb9c
Joshua Isaac <hello.joshua@hotmail.com>
2021-03-26 20:41:49 -0400

    darken textblockwimage module to pass lighthouse score

--- components/agility-modules/TextBlockWithImage.js
+++ components/agility-modules/TextBlockWithImage.js
@@ -37,7 +37,7 @@ const TextBlockWithImage = ({ module }) => {
             <h2 className="font-display text-4xl font-black text-secondary-500 md:text-3xl lg:text-5xl tracking-wide text-center mt-4 lg:leading-tight md:text-left">
               {fields.title}
             </h2>
-            <p className="mt-4 text-center md:text-left text-sm md:text-base lg:text-lg font-medium leading-relaxed text-secondary-100">
+            <p className="mt-4 text-center md:text-left text-sm md:text-base lg:text-lg font-medium leading-relaxed text-secondary-200">
               {fields.content}
             </p>
             {fields.primaryButton && (

e36f2df8e1545098154bc7c4d2fe583c4a9952bc
joshua <40035381+joshua-isaac@users.noreply.github.com>
2021-04-09 11:01:32 -0400

    Update Layout.js

--- components/common/Layout.js
+++ components/common/Layout.js
@@ -1,5 +1,5 @@
 import { getPageTemplate } from "components/agility-pageTemplates";
-// import { handlePreview } from "@agility/nextjs";
+import { handlePreview } from "@agility/nextjs";
 import { useRouter } from "next/router";
 import Error from "next/error";
 import PreviewBar from "./PreviewBar";
@@ -8,41 +8,7 @@ import SiteHeader from "./SiteHeader";
 import SiteFooter from "./SiteFooter";
 import LoadingWidget from "./LoadingWidget";
 
-const getParameterByName = (name) => {
-  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
-  var results = regex.exec(location.search);
-  return results === null
-    ? ""
-    : decodeURIComponent(results[1].replace(/\+/g, " "));
-};
-const handlePreview = () => {
-  if (!process.browser) {
-    //kickout if this is not being executed in the browser
-    return false;
-  }
-  const agilityPreviewKey = getParameterByName(`agilitypreviewkey`);
-  if (!agilityPreviewKey) {
-    //kickout if we don't have a preview key
-    return false;
-  }
-  //redirect this to our preview API route
-  const previewAPIRoute = `/api/preview`;
-  let previewAPIUrl = `${previewAPIRoute}?slug=${window.location.pathname}&agilitypreviewkey=${agilityPreviewKey}`;
-  const dynamicPageContentID = parseInt(
-    getParameterByName("ContentID") ?? getParameterByName("contentID")
-  );
-  if (dynamicPageContentID > 0) {
-    previewAPIUrl += `&ContentID=${dynamicPageContentID}`;
-  }
-  console.log("Activating preview", previewAPIUrl);
-  //do the redirect
-  setTimeout(function () {
-    window.location.href = previewAPIUrl;
-  }, 2500);
-  return true;
-};
-
+// set up handle preview
 const isPreview = handlePreview();
 
 function Layout(props) {

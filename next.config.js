12387574efb54c5cd1cd5d926b12d71155b02882
Joel Varty <joel@agilitycms.com>
2021-06-24 10:18:42 -0400

    config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,3 @@
 module.exports = {
-	// Here we configure and set up the Agility CDN domain config so we can take advantage of the next/image component
-	// Read more about next/image here: https://nextjs.org/docs/api-reference/next/image
-	images: {
-
-		domains: [
-			`cdn.aglty.io`,
-			`cdn-dev.aglty.io`,
-			`${process.env.AGILITY_GUID}-cdn.agilitycms.cloud`,
-		],
-	}
+	target: "serverless"
 };

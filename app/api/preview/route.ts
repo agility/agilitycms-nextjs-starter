3c0dc10314b9e4cbc4123525f4ae053e65fd1714
Joel Varty <joel@agilitycms.com>
2024-06-17 14:24:08 -0400

    Improved perf for getting into preview mode

--- app/api/preview/route.ts
+++ app/api/preview/route.ts
@@ -2,13 +2,27 @@ import { validatePreview, getDynamicPageURL } from "@agility/nextjs/node";
 import { draftMode } from 'next/headers'
 import { NextRequest, NextResponse } from "next/server";
 
+
+/**
+ * This endpoint is used as a rewrite for preview requests from middleware.
+ * Therefore, the URL will be the original preview request url...
+ * @param request
+ * @param res
+ * @returns
+ */
 export async function GET(request: NextRequest, res: NextResponse) {
 
 	const searchParams = request.nextUrl.searchParams
 
-	const agilityPreviewKey = searchParams.get('agilitypreviewkey')
-	const slug = searchParams.get('slug')
+	const agilityPreviewKey = searchParams.get("agilitypreviewkey") || ""
+
+	//locale is also passed in the querystring on preview requests
+	const locale = searchParams.get("lang")
+	const slug = request.nextUrl.pathname
+
 	const ContentID = searchParams.get('ContentID')
+
+	console.log("Preview Request", { agilityPreviewKey, slug, ContentID, url: request.nextUrl.toString() })
 
 
 	//validate our preview key, also validate the requested page to preview exists
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
+import {draftMode} from "next/headers"
 import LoadingWidget from "components/common/LoadingWidget"
 import PreviewBar from "components/common/PreviewBar"
 import SiteFooter from "components/common/SiteFooter"
 import SiteHeader from "components/common/SiteHeader"
+
 import {useAgilityContext} from "lib/cms/useAgilityContext"
 
 import {Inter} from "next/font/google"
@@ -9,6 +11,8 @@ import {Inter} from "next/font/google"
 import "/styles/globals.css"
 
 import {getHeaderContent} from "lib/cms-content/getHeaderContent"
+import {redirect} from "next/navigation"
+import {NextRequest} from "next/server"
 
 const inter = Inter({
 	subsets: ["latin"],
@@ -20,24 +24,36 @@ export default async function RootLayout({children}: {children: React.ReactNode}
 
 	const header = await getHeaderContent({sitemap, locale})
 
-	const isPreviewRequested = false
+	async function startPreviewMode(pathname: string) {
+		"use server"
+
+		//turn on draft/preview mode
+		draftMode().enable()
+
+		// Redirect to the same page
+		let url = `${pathname}`
+		if (url.includes("?")) {
+			url = `${url}&preview=1`
+		} else {
+			url = `${url}?preview=1`
+		}
+		redirect(url)
+	}
+
 	return (
 		<html lang="en" className={inter.className}>
 			<body data-agility-guid={process.env.AGILITY_GUID}>
 				<div id="site-wrapper">
-					{isPreviewRequested && <LoadingWidget message="Loading Preview Mode" />}
-					{!isPreviewRequested && (
-						<div id="site">
-							<PreviewBar {...{isDevelopmentMode, isPreview}} />
+					<div id="site">
+						<PreviewBar {...{isDevelopmentMode, isPreview, startPreviewMode}} />
 
-							<div className="flex flex-col min-h-screen">
-								<SiteHeader {...{header}} />
+						<div className="flex flex-col min-h-screen">
+							<SiteHeader {...{header}} />
 
-								<main className={`flex-grow`}>{children}</main>
-								<SiteFooter />
-							</div>
+							<main className={`flex-grow`}>{children}</main>
+							<SiteFooter />
 						</div>
-					)}
+					</div>
 				</div>
 			</body>
 		</html>

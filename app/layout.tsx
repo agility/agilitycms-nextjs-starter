2949c988b57e14dcdd6027ec02cc4351e2b4aabf
Dakotah Godfrey <dakotah.godfrey.dev@gmail.com>
2024-09-04 10:55:30 -0400

    Decorator tweaks for sdk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,61 +1,69 @@
-import {draftMode} from "next/headers"
+import { draftMode } from "next/headers"
 import LoadingWidget from "components/common/LoadingWidget"
 import PreviewBar from "components/common/PreviewBar"
 import SiteFooter from "components/common/SiteFooter"
 import SiteHeader from "components/common/SiteHeader"
 
-import {useAgilityContext} from "lib/cms/useAgilityContext"
+import { useAgilityContext } from "lib/cms/useAgilityContext"
 
-import {Inter} from "next/font/google"
+import { Inter } from "next/font/google"
 
 import "/styles/globals.css"
 
-import {getHeaderContent} from "lib/cms-content/getHeaderContent"
-import {redirect} from "next/navigation"
-import {NextRequest} from "next/server"
+import { getHeaderContent } from "lib/cms-content/getHeaderContent"
+import { redirect } from "next/navigation"
+import { NextRequest } from "next/server"
+import Script from "next/script"
 
 const inter = Inter({
-	subsets: ["latin"],
-	variable: "--font-inter",
+  subsets: ["latin"],
+  variable: "--font-inter",
 })
 
-export default async function RootLayout({children}: {children: React.ReactNode}) {
-	const {locale, sitemap, isDevelopmentMode, isPreview} = useAgilityContext()
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const { locale, sitemap, isDevelopmentMode, isPreview } = useAgilityContext()
 
-	const header = await getHeaderContent({sitemap, locale})
+  const header = await getHeaderContent({ sitemap, locale })
 
-	async function startPreviewMode(pathname: string) {
-		"use server"
+  async function startPreviewMode(pathname: string) {
+    "use server"
 
-		//turn on draft/preview mode
-		draftMode().enable()
+    //turn on draft/preview mode
+    draftMode().enable()
 
-		// Redirect to the same page
-		let url = `${pathname}`
-		if (url.includes("?")) {
-			url = `${url}&preview=1`
-		} else {
-			url = `${url}?preview=1`
-		}
-		redirect(url)
-	}
+    // Redirect to the same page
+    let url = `${pathname}`
+    if (url.includes("?")) {
+      url = `${url}&preview=1`
+    } else {
+      url = `${url}?preview=1`
+    }
+    redirect(url)
+  }
 
-	return (
-		<html lang="en" className={inter.className}>
-			<body data-agility-guid={process.env.AGILITY_GUID}>
-				<div id="site-wrapper">
-					<div id="site">
-						<PreviewBar {...{isDevelopmentMode, isPreview, startPreviewMode}} />
+  return (
+    <html lang="en" className={inter.className}>
+      <body data-agility-guid={process.env.AGILITY_GUID}>
+        <div id="site-wrapper">
+          <div id="site">
+            <PreviewBar
+              {...{ isDevelopmentMode, isPreview, startPreviewMode }}
+            />
 
-						<div className="flex flex-col min-h-screen">
-							<SiteHeader {...{header}} />
+            <div className="flex flex-col min-h-screen">
+              <SiteHeader {...{ header }} />
 
-							<main className={`flex-grow`}>{children}</main>
-							<SiteFooter />
-						</div>
-					</div>
-				</div>
-			</body>
-		</html>
-	)
+              <main className={`flex-grow`}>{children}</main>
+              <SiteFooter />
+            </div>
+          </div>
+        </div>
+      </body>
+      <Script src="https://unpkg.com/@agility/web-studio-sdk@latest/dist/index.js" />
+    </html>
+  )
 }
--- components/agility-components/PostDetails.tsx
+++ components/agility-components/PostDetails.tsx
@@ -1,50 +1,76 @@
 import React from "react"
-import {AgilityPic, UnloadedModuleProps, renderHTML} from "@agility/nextjs"
+import { AgilityPic, UnloadedModuleProps, renderHTML } from "@agility/nextjs"
 
-import {DateTime} from "luxon"
+import { DateTime } from "luxon"
 
-const PostDetails = async ({dynamicPageItem}: UnloadedModuleProps) => {
-	if (!dynamicPageItem) {
-		return <div>Post not found</div>
-	}
+const PostDetails = async ({ dynamicPageItem }: UnloadedModuleProps) => {
+  if (!dynamicPageItem) {
+    return <div>Post not found</div>
+  }
 
-	// post fields
-	const post = dynamicPageItem.fields
+  // post fields
+  const post = dynamicPageItem.fields
 
-	// category
-	const category = post.category?.fields.title || "Uncategorized"
+  // category
+  const category = post.category?.fields.title || "Uncategorized"
 
-	// format date
-	const dateStr = DateTime.fromJSDate(new Date(post.date)).toFormat("LLL. dd, yyyy")
+  // format date
+  const dateStr = DateTime.fromJSDate(new Date(post.date)).toFormat(
+    "LLL. dd, yyyy"
+  )
 
-	return (
-		<>
-			<div className="relative px-8">
-				<div className="max-w-screen-xl mx-auto">
-					<div className="h-64 md:h-96  lg:h-[480px] relative overflow-hidden rounded-lg">
-						<AgilityPic
-							image={post.image}
-							alt={post.image.label}
-							className="w-full"
-							fallbackWidth={800}
-							sources={[
-								{media: "(min-width: 1280px)", width: 1200},
-								{media: "(min-width: 640px)", width: 800},
-								{media: "(max-width: 639px)", width: 640},
-							]}
-						/>
-					</div>
-					<div className="max-w-2xl mx-auto mt-4">
-						<div className="uppercase text-primary-500 text-xs font-bold tracking-widest leading-loose">{category}</div>
-						<div className="border-b-2 border-primary-500 w-8"></div>
-						<div className="mt-4 uppercase text-gray-600 italic font-semibold text-xs">{dateStr}</div>
-						<h1 className="font-display text-4xl font-bold my-6 text-secondary-500">{post.title}</h1>
-						<div className="prose max-w-full mb-20" dangerouslySetInnerHTML={renderHTML(post.content)} />
-					</div>
-				</div>
-			</div>
-		</>
-	)
+  // content id
+  const contentID = dynamicPageItem.contentID
+
+  return (
+    <>
+      <div className="relative px-8" data-agility-component={contentID}>
+        <div className="max-w-screen-xl mx-auto">
+          <div className="h-64 md:h-96  lg:h-[480px] relative overflow-hidden rounded-lg">
+            <AgilityPic
+              data-agility-field="image"
+              image={post.image}
+              alt={post.image.label}
+              className="w-full"
+              fallbackWidth={800}
+              sources={[
+                { media: "(min-width: 1280px)", width: 1200 },
+                { media: "(min-width: 640px)", width: 800 },
+                { media: "(max-width: 639px)", width: 640 },
+              ]}
+            />
+          </div>
+          <div className="max-w-2xl mx-auto mt-4">
+            <div
+              data-agility-field="category"
+              className="uppercase text-primary-500 text-xs font-bold tracking-widest leading-loose"
+            >
+              {category}
+            </div>
+            <div className="border-b-2 border-primary-500 w-8"></div>
+            <div
+              data-agility-field="date"
+              className="mt-4 uppercase text-gray-600 italic font-semibold text-xs"
+            >
+              {dateStr}
+            </div>
+            <h1
+              data-agility-field="title"
+              className="font-display text-4xl font-bold my-6 text-secondary-500"
+            >
+              {post.title}
+            </h1>
+            <div
+              data-agility-field="content"
+              data-agility-html="true"
+              className="prose max-w-full mb-20"
+              dangerouslySetInnerHTML={renderHTML(post.content)}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default PostDetails

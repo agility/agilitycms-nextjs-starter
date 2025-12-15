0f53fb08e9ae9e803c4c60c1888004a8d9d20f10
AaronAgility <aaron@agilitycms.com>
2025-12-15 09:21:26 -0500

    Middleware to prox

--- proxy.ts
+++ proxy.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from 'next/server'
 import { getDynamicPageURL } from "@agility/nextjs/node"
 
 // This function can be marked `async` if using `await` inside
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
 
 
 	/*****************************
